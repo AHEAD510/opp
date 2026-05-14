b9f2adf6493548c3b1e6a77823aeb6ab3897b294
Kunihiro Goto <goto@ahead.wine>
2026-05-14 22:51:19 +0900

    マイクロCMS0514

---
+++ assets/js/cms-config.js
@@ -0,0 +1,7 @@
+// microCMS connection settings
+// API キーは microCMS 管理画面の「API キー」から取得し、ここに設定してください。
+// 公開リポジトリの場合はこのファイルを .gitignore に追加することを推奨します。
+const CMS_CONFIG = {
+  serviceDomain: 'opp',
+  apiKey: 'ltP3NfqS6L5OhoxIy4vp6rm0a8e0SQ8HCoS5'
+};
